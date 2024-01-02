0c2074ec0aa2413adb29ad2f7ff442659e501f36
hirohata <hirohatatro@gmail.com>
2024-01-02 10:55:03 -0800

    feat: added a behavior when fetched accounts

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,46 @@
 $(() => {
   //Start coding here!
-  $.get("http://localhost:3000/accounts").done((data) =>
-    console.log("data", data)
-  );
+  fetchAccounts();
 });
+
+/**
+ * get accounts from the server.
+ * If the request is successful, the data will be rendered in the select tag of the Account section.
+ */
+const fetchAccounts = () => {
+  fetch("http://localhost:3000/accounts", {
+    method: "GET"
+  }).then(res => {
+    res.json().then(data => {
+      // generate option tags
+      const optionTags = () => data.map(account => generateOptionTag(account.id, account.username));
+
+      // grab the account select tag from the DOM.
+      const accountFilterSelectTag = $("#account-filter");
+      // generate default option tag.
+      const accountNotSelected = "<option value='0'>ALL</option>"
+      accountFilterSelectTag.append([accountNotSelected, ...optionTags()]);
+
+      // grab the account select tag from the DOM.
+      const accountSelectTag = $("#account");
+      // add option tags to the select tag.
+      accountSelectTag.append(optionTags());
+    })
+  }).catch(e => {
+      console.error(e)
+    }
+  )
+}
+
+/**
+ * generate option tags for the select tag.
+ * @param {number} id
+ * @param {string} name
+ * @returns {HTMLOptionElement} optionTag
+ */
+const generateOptionTag = (id, name) => {
+  const optionTag = document.createElement("option");
+        optionTag.value = id.toString();
+        optionTag.innerText = name;
+  return optionTag;
+}
