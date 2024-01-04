1e247d248d5c65a21d129269fbbdd3afe6c309a0
hirohata <hirohatatro@gmail.com>
2024-01-04 12:29:43 -0800

    feat: added POST /transactions

--- frontend/js/apis/transactions.js
+++ frontend/js/apis/transactions.js
@@ -23,3 +23,50 @@ export const fetchTransactions = () => {
       });
   });
 };
+
+/**
+ * @typedef IAddTransactionInput
+ * @param {number | null} accountId
+ * @param {number | null} accountIdFrom
+ * @param {number | null} accountIdTo
+ * @param {'Deposit' | 'Withdrow' | 'Transfer'} type
+ * @param {number} amount
+ * @param {number} categoryId
+ * @param {string} description
+ */
+
+/**
+ * Add transaction to the account.
+ * @param {IAddTransactionInput} input - IAddTransactionInput object.
+ */
+export const addNewTransaction = (input) => {
+  const { accountId, accountIdFrom, accountIdTo, type, amount, categoryId, description } = input;
+
+  const transactionPayload = {
+    newTransaction: {
+      accountId: accountId ? Number(accountId) : null,
+      accountIdFrom: accountIdFrom ? Number(accountIdFrom) : null,
+      accountIdTo: accountIdTo ? Number(accountIdTo) : null,
+      type,
+      amount,
+      categoryId: Number(categoryId),
+      description,
+    },
+  };
+
+  fetch('http://localhost:3000/transactions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(transactionPayload),
+  })
+    .then((res) => {
+      res.json().then((data) => {
+        store.addTransactions(data);
+      });
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+};
