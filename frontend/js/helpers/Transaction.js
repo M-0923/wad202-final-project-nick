6a26bc5b515d90a88355a0f4714506434a890a27
hirohata <hirohatatro@gmail.com>
2024-01-03 16:43:17 -0800

    feat: implemented the Transactions model

--- frontend/js/helpers/Transaction.js
+++ frontend/js/helpers/Transaction.js
@@ -1,23 +1,76 @@
-class Transaction {
-  constructor(amount, account) {
-    this.amount = amount;
-    this.account = account;
-  }
-  commit() {
-    if (this.value < 0 && this.amount > this.account.balance) return;
-    this.account.transactions.push(this.value);
-    // this.account.balance += this.value;
-  }
-}
+/**
+ * @typedef ITransaction
+ * @property {number} id
+ * @property {number} accountId
+ * @property {number} accountIdFrom
+ * @property {number} accountIdTo
+ * @property {'Deposit' | 'Withdraw' | 'Transfer'} type
+ * @property {number} amount
+ * @property {number} categoryId
+ * @property {string} description
+ */
+class Transactions {
+  /**
+   * @type {ITransaction[]} - Array of Transaction objects.
+   */
+  #transactions = [];
 
-class Withdrawal extends Transaction {
-  get value() {
-    return -this.amount;
+  /**
+   * @return {ITransaction[]}
+   */
+  get transactions() {
+    return this.#transactions;
   }
-}
 
-class Deposit extends Transaction {
-  get value() {
-    return this.amount;
+  /**
+   * add a new transaction to the array of transactions in this class property.
+   * @param {ITransaction} transaction - Transaction object.
+   */
+  addTransaction(transaction) {
+    this.#transactions.push(transaction);
+  }
+
+  /**
+   * get a balance of the account.
+   * @param {Account} account - Account object.
+   * @return {number} - Balance of the account.
+   */
+  getBalance(account) {
+    const accountId = account.id;
+    const relatedTransaction = this.#transactions.filter(
+      (transaction) => transaction.accountId === accountId,
+    ); // filter transaction by the account
+
+    // account's amount.
+    // this is calculated by the sum of the transfer, deposits and withdraws of the account.
+    let accountsAmount = 0;
+
+    for (const transaction of relatedTransaction) {
+      if (transaction.type === 'Deposit') {
+        accountsAmount += transaction.amount;
+        continue;
+      }
+
+      if (transaction.type === 'Withdraw') {
+        accountsAmount -= transaction.amount;
+        continue;
+      }
+
+      // the following code is for transfer transaction.
+      transaction.accountIdFrom === accountId
+        ? (accountsAmount -= transaction.amount)
+        : (accountsAmount += transaction.amount);
+    }
+
+    return accountsAmount;
+  }
+
+  /**
+   * get all transactions of the account.
+   * @param {Account} account - Account instance.
+   * @return {ITransaction[]} - Array of Transaction objects.
+   */
+  getTransactionsByAccountId(account) {
+    return this.#transactions.filter((transaction) => transaction.accountId === account.id);
   }
 }
